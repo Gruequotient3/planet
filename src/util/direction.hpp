d483c5b2f624cdd87f82971bec7682ac9f8b6a44
LEVASTRE Clément <levastre.clement@gmail.com>
2025-02-11 09:04:37 +0100

    Add direction vector selection

--- src/util/direction.hpp
+++ src/util/direction.hpp
@@ -3,6 +3,18 @@
 
 enum Direction {NORTH, SOUTH, EAST, WEST, UP, DOWN};
 
+static inline glm::vec3 getDirectionVector(enum Direction direction){
+    glm::vec3 directionVector[] = {
+        glm::vec3( 0.0f,  0.0f, -1.0f), // north (-z)
+        glm::vec3( 0.0f,  0.0f,  1.0f), // south (+z)
+        glm::vec3( 1.0f,  0.0f,  0.0f), // east (+x)
+        glm::vec3(-1.0f,  0.0f,  0.0f), // west (-x)
+        glm::vec3( 0.0f,  1.0f,  0.0f), // up (+y)
+        glm::vec3( 0.0f, -1.0f,  0.0f), // down (-y)
+    };
+    return directionVector[direction];
+
+}
 
 
 #endif
