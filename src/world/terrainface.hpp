03733d7ea1a745fcf9252ff2a342d7bb03d6265a
LEVASTRE Clément <levastre.clement@gmail.com>
2025-03-07 12:36:42 +0100

    Adapt shapeSettings class for new noiseFilter parameter and multiple noiseFilter handling

--- src/world/terrainface.hpp
+++ src/world/terrainface.hpp
@@ -3,17 +3,32 @@
 
 #include <glm/glm.hpp>
 
+#include <vector>
+
 #include "../util/mesh.hpp"
 #include "../noise/noisefilter.hpp"
 
 class ShapeSettings{
     public:
         float radius;
-        NoiseFilter noiseFilter;
-        ShapeSettings(float radius = 1.0f){this->radius = radius;}
+        std::vector<NoiseFilter> noiseFilter;
+        ShapeSettings(float radius = 1.0f) :
+            radius{radius}{}
         glm::vec3 CalculatePointOnPlanet(glm::vec3 pointOnUnitSphere){
-            float elevation = noiseFilter.Evaluate(pointOnUnitSphere);
-            return pointOnUnitSphere * radius * (1 + elevation);
+            float firstLayerValue = 0.0f;
+            float elevation = 0.0f;
+
+            if (noiseFilter.size() > 0){
+                firstLayerValue = noiseFilter[0].Evaluate(pointOnUnitSphere);
+                if (noiseFilter[0].enable) elevation = firstLayerValue;
+            }
+            for (int i = 1; i < (int)noiseFilter.size(); ++i){
+                if (noiseFilter[i].enable){
+                    float mask = noiseFilter[i].useFirstLayerAsMask ? firstLayerValue : 1;
+                    elevation += noiseFilter[i].Evaluate(pointOnUnitSphere) * mask;
+                }
+            }
+            return pointOnUnitSphere * radius * (1+elevation);
         }
 
 };
