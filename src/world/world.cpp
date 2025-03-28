31525a478affd022e8a50531b857b9b1114b6c05
LEVASTRE Clément <levastre.clement@gmail.com>
2025-03-28 10:57:50 +0100

    Add world class

---
+++ src/world/world.cpp
@@ -0,0 +1,108 @@
+#ifndef WORLD_CPP
+#define WORLD_CPP
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "world.hpp"
+
+#include <iostream>
+#include <cstdlib>
+
+Camera World::camera = Camera{};
+World::World() : planetShader{Shader{"res/shaders/planet.vert", "res/shaders/planet.frag"}},
+                 skyShader{Shader{"res/shaders/sky.vert", "res/shaders/sky.frag"}}
+{
+    glActiveTexture(GL_TEXTURE0);
+    skyTexture = Texture("res/textures/sky.jpg", GL_RGB);
+    planets = nullptr;
+    datas = nullptr;
+    Init();
+}
+
+World::~World(){
+    // Sky
+    sky->~Sky();
+
+    // Planets
+    for (int i = 0; i < nbPlanet; ++i){
+        planets[i].~Planet();
+        datas[2 * i].~Data();
+        datas[2 * i + 1].~Data();
+
+    }
+    free(sky);
+    free(planets);
+    free(datas);
+}
+
+void World::Init(){
+    // Sky
+    sky = (Sky*) malloc(sizeof(Sky));
+    new (sky) Sky{skyShader, skyTexture, 8};
+    sky->ConstructMesh();
+
+    // Planets
+    nbPlanet = (int)(sizeof(dataSettingPaths) / (sizeof(std::string) * 2));
+    planets = (Planet*)malloc(sizeof(Planet) * nbPlanet);
+    datas = (Data*) malloc(sizeof(Data) * nbPlanet * 2);
+    for (int i = 0; i < nbPlanet; ++i){
+        new (planets+i) Planet{planetShader};
+        new (datas+ 2 * i) Data();
+        new (datas+ 2 * i +1) Data();
+
+        if (i == 0) planets[i].type = SUN;
+
+        datas[2 * i].LoadData(dataSettingPaths[2 * i]);
+        datas[2 * i + 1].LoadData(dataSettingPaths[2 * i + 1]);
+        planets[i].SetSettingsFromData(datas[2 * i]);
+        planets[i].SetColorsFromData(datas[2 * i + 1]);
+        planets[i].position = glm::vec3(i * 10.f, 0.0f, 0.0f);
+    }
+
+
+    planetShader.use();
+    planetShader.setVec3("lightPos", lightPos);
+    planetShader.setVec3("lightColor", lightColor);
+}
+
+void World::Update(float deltaTime, UpdateMode updateMode){
+    static float theta = 0.0f;
+    static float time = 0.0f;
+    glm::mat4 model;
+
+    // Update Data
+    if (updateMode == UPDATE_DATA){
+        if (time >= 0.5f){
+            for (int  i = 0, id = 0; i < nbPlanet; ++i, id += 2){
+                if (!datas[id].UpdateData()) planets[i].SetSettingsFromData(datas[id]);
+                if (!datas[id+1].UpdateData()) planets[i].SetColorsFromData(datas[id+1]);
+            }
+            time = 0.0f;
+        }
+    }
+
+    // Sky
+    sky->position = camera.position;
+    sky->Draw();
+
+    // Move planets
+    for (int i = 0; i < nbPlanet; ++i){
+        model = glm::mat4(1.0f);
+        planetShader.use();
+        if (planets[i].type != SUN){
+            model = glm::rotate(model, glm::radians(theta * planets[i].revolutionSpeed),
+                                glm::vec3(0.0f , 1.0f, 0.0f));
+        }
+        model = glm::translate(model, planets[i].position);
+        model = glm::rotate(model, glm::radians(theta * planets[i].rotationSpeed),
+                            glm::normalize(glm::vec3(0.0f , 1.0f, 0.0f)));
+
+        planetShader.setMat4("model", GL_FALSE, glm::value_ptr(model));
+        planets[i].Draw();
+    }
+    theta += deltaTime * 360.0f;
+    if (updateMode != NO_UPDATE) time += deltaTime;
+}
+
+#endif
---
+++ src/world/world.hpp
@@ -0,0 +1,44 @@
+#ifndef WORLD_HPP
+#define WORLD_HPP
+
+#include <string>
+#include <vector>
+
+#include "planet.hpp"
+#include "../util/data.hpp"
+#include "sky.hpp"
+
+enum UpdateMode {NO_UPDATE, UPDATE_DATA};
+
+static const std::string dataSettingPaths[]{
+    "res/planetConfig/settings/p1.txt", "res/planetConfig/color/p1.txt",
+    "res/planetConfig/settings/p2.txt", "res/planetConfig/color/p2.txt",
+    "res/planetConfig/settings/p3.txt", "res/planetConfig/color/p3.txt",
+    "res/planetConfig/settings/p4.txt", "res/planetConfig/color/p4.txt",
+};
+
+static const glm::vec3 lightPos = glm::vec3(0.0f, 0.0f, 0.0f);
+static const glm::vec3 lightColor = glm::vec3(1.0f, 0.96f, 0.57f);
+
+class World{
+    private:
+        Sky* sky;
+
+        int nbPlanet;
+        Planet* planets;
+        Data* datas;
+
+        Shader planetShader;
+        Shader skyShader;
+
+        Texture skyTexture;
+        void Init();
+
+    public:
+        static Camera camera;
+        World();
+        ~World();
+        void Update(float deltaTime, UpdateMode updateMode);
+};
+
+#endif
